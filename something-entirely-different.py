44967cc6e2601a57f15e10d89cbbd994a526c1fe
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-06 09:13:12 -0400

    Update something-entirely-different.py

--- something-entirely-different.py
+++ something-entirely-different.py
@@ -1,1 +1,6 @@
-
+def fullstring():
+  fullstring=f '(fact1)(fact2)'
+  return fullstring
+fact1='I can bend my fingers almost 180 degrees in each direction'
+fact2='I have the best cat'
+print(fullstring)
