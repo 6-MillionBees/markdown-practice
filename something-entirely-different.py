ce875916336f899cd7f974a75ebe31c2fb1be608
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-06 09:23:23 -0400

    Delete something-entirely-different.py

--- something-entirely-different.py
+++
@@ -1,6 +0,0 @@
-def fullstring():
-  fullstring=f '(fact1)(fact2)'
-  return fullstring
-fact1='I can bend my fingers almost 180 degrees in each direction'
-fact2='I have the best cat'
-print(fullstring)
